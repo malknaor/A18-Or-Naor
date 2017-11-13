8774b5c9dfc642f382a57f44616aff7c6fe1c97c
malknaor <malknaor@gmail.com>
2017-11-13 10:01:21 +0200

    First Commit

---
+++ A18 Ex01 Or 200337251 Naor 301032157/Form1.cs
@@ -0,0 +1,113 @@
+﻿using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace A18_Ex01_Or_200337251_Naor_301032157
+{
+    public partial class formFacebookApp : Form
+    {
+        private const string k_AppID = "135637767091578";
+
+        User m_LoggedInUser;
+
+        public formFacebookApp()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUserLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
+        }
+
+        private void loginAndInit()
+        {
+            /// Owner: design.patterns
+
+            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
+            /// You can then save the result.AccessToken for future auto-connect to this user:
+            LoginResult result = FacebookService.Login(k_AppID, /// (desig patter's "Design Patterns Course App 2.4" app)
+                "public_profile",
+                "user_education_history",
+                "user_birthday",
+                "user_actions.video",
+                "user_actions.news",
+                "user_actions.music",
+                "user_actions.fitness",
+                "user_actions.books",
+                "user_about_me",
+                "user_friends",
+                "publish_actions",
+                "user_events",
+                "user_games_activity",
+                //"user_groups" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_hometown",
+                "user_likes",
+                "user_location",
+                "user_managed_groups",
+                "user_photos",
+                "user_posts",
+                "user_relationships",
+                "user_relationship_details",
+                "user_religion_politics",
+
+                //"user_status" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_tagged_places",
+                "user_videos",
+                "user_website",
+                "user_work_history",
+                "read_custom_friendlists",
+
+                // "read_mailbox", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "read_page_mailboxes",
+                // "read_stream", (This permission is only available for apps using Graph API version v2.3 or older.)
+                // "manage_notifications", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "manage_pages",
+                "publish_pages",
+                "publish_actions",
+
+                "rsvp_event"
+                );
+            // These are NOT the complete list of permissions. Other permissions for example:
+            // "user_birthday", "user_education_history", "user_hometown", "user_likes","user_location","user_relationships","user_relationship_details","user_religion_politics", "user_videos", "user_website", "user_work_history", "email","read_insights","rsvp_event","manage_pages"
+            // The documentation regarding facebook login and permissions can be found here: 
+            // https://developers.facebook.com/docs/facebook-login/permissions#reference
+
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                fetchUserInfo();
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+        private void fetchUserInfo()
+        {
+
+            pictureBoxProfilePhoto.LoadAsync(m_LoggedInUser.PictureNormalURL);
+
+            if (m_LoggedInUser.Posts.Count > 0)
+            {
+             //   textBoxStatus.Text = m_LoggedInUser.Posts[0].Message;
+            }
+        }
+
+        private void pictureBoxProfilePhoto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ A18 Ex01 Or 200337251 Naor 301032157/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A18_Ex01_Or_200337251_Naor_301032157
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new formFacebookApp());
+        }
+    }
+}
